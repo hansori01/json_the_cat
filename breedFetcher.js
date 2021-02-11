d75441951870b93b5bbdbdd191fbb53e6574aa7d
hansori01 <19sorihan88@gmail.com>
2021-02-11 21:13:45 +0000

    updated error hoisting note

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,8 +4,10 @@ const args = process.argv.slice(2);
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${args}`
 
 request(url, (error, response, body) => {
+  // this error is at top because if not, the const data = JSON... will throw the error first
   if (error) {
     console.log('error:', error);
+    //if we hit error then exit request
     return;
   }
   const data = JSON.parse(body)[0];
@@ -16,7 +18,4 @@ request(url, (error, response, body) => {
     const description = data['description']
     console.log(description);
   }
-  // console.log('body:', body); // body used to be string. data is now the object
-  //find out the type of body. It is a string! //confirm that data is now object.
-  // console.log(typeof body + ' ' + typeof data);
 });
